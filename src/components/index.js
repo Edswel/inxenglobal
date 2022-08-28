96f02997961f214272b4e8b8d365f0effbbde924
Edswel <franckini@yahoo.com>
2022-08-28 20:50:12 +0100

    Add all needed components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Clients from './Clients';
+import Navbar from './Navbar';
+import Billing from './Billing';
+import Business from './Business';
+import CardDeal from './CardDeal';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import FeedbackCard from './FeedbackCard';
+import Button from './Button';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+
+export { Clients, Navbar, Billing, Business, Button, CardDeal, CTA, FeedbackCard, Stats, Footer, Testimonials, Hero };
