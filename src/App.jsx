2415c7d1f64ce08f82794f8bcb8250a4c96ff07f
Edswel <franckini@yahoo.com>
2022-08-28 20:08:20 +0100

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import styles from './styles';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+    </div>
   )
 }
 
