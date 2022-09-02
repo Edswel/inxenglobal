611f018bd6b1c5bd9c129a8cccf7e423fce3d5b8
Edswel <franckini@yahoo.com>
2022-09-02 11:41:54 +0100

    Build lower part of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { logo } from '../assets';
-import { footerLinks } from '../constants';
+import { footerLinks, socialMedia } from '../constants';
 import styles from '../styles';
 
 const Footer = () => {
@@ -21,6 +21,14 @@ const Footer = () => {
                                 ))}
                             </ul>
                         </div>
+                    ))}
+                </div>
+            </div>
+            <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
+                <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>2022 Hoobank. All Rights Reserved.</p>
+                <div className='flex flex-row md:mt-0 mt-6'>
+                    {socialMedia.map((social, index) => (
+                        <img className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`} src={social.icon} alt={social.id} key={social.id} />
                     ))}
                 </div>
             </div>
